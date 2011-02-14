34fdc7347ea6871de58312421d9249063cacc53e
Anthony Garcia <lagg@lavabit.com>
2011-02-14 11:38:39 -0700

    Use str instead of repr in ProfileError.__str__

--- steam/user.py
+++ steam/user.py
@@ -24,7 +24,7 @@ class ProfileError(Exception):
         self.msg = msg
 
     def __str__(self):
-        return repr(self.msg)
+        return str(self.msg)
 
 class profile:
     """ Functions for reading user account data """

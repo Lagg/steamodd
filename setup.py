3467b8b49613f8941fc5c97cca8706b15ef6ca9e
Anthony Garcia <anthony@lagg.me>
2013-06-01 19:27:33 -0700

    Initial STI code (Steamodd Testing Initiative)
    Removes old and incompatible test.py script, adds empty tests
    dir/module.
    
    Adds custom distutils command to run unit tests, uses discovery. Just
    write tests/test*.py files and you're good to go. (See finalize_options
    for an example of how to get command opts into a usable form)

--- setup.py
+++ setup.py
@@ -3,8 +3,31 @@ Copyright (c) 2010-2013, Anthony Garcia <anthony@lagg.me>
 Distributed under the ISC License (see LICENSE)
 """
 
-from distutils.core import setup
+from distutils.core import setup, Command
+from distutils.errors import DistutilsOptionError
+from unittest import TestLoader, TextTestRunner
+import os
 import steam
+
+class run_tests(Command):
+    description = "Run the steamodd unit tests"
+
+    user_options = [
+            ("key=", 'k', "Your API key")
+            ]
+
+    def initialize_options(self):
+        self.key = None
+
+    def finalize_options(self):
+        if not self.key:
+            raise DistutilsOptionError("API key is required")
+        else:
+            os.environ["STEAM_API_KEY"] = self.key
+
+    def run(self):
+        tests = TestLoader().discover("tests")
+        TextTestRunner(verbosity = 2).run(tests)
 
 setup(name = "steamodd",
       version = steam.__version__,
@@ -19,4 +42,5 @@ setup(name = "steamodd",
           "Operating System :: OS Independent",
           "Programming Language :: Python"
           ],
-      license = steam.__license__)
+      license = steam.__license__,
+      cmdclass = {"run_tests": run_tests})

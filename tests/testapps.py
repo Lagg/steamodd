5655a046d90fbcbd76bce623a8a42f9fa211cad8
Anthony Garcia <anthony@lagg.me>
2013-06-18 15:22:43 -0700

    Use setUpClass where appropriate

--- tests/testapps.py
+++ tests/testapps.py
@@ -2,11 +2,15 @@ import unittest
 from steam import apps
 
 class AppsTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._apps = apps.app_list()
+
     def test_app_list_get_tf2(self):
-        self.assertEquals(apps.app_list()[440], (440, 'Team Fortress 2'))
+        self.assertEquals(self._apps[440], (440, 'Team Fortress 2'))
 
     def test_app_list_get_dota2(self):
-        self.assertEquals(apps.app_list()[570], (570, 'DOTA 2'))
+        self.assertEquals(self._apps[570], (570, 'DOTA 2'))
 
     def test_app_list_get_invalid_app(self):
-        self.assertRaises(apps.AppError, apps.app_list()[12345678])
+        self.assertRaises(apps.AppError, self._apps[12345678])
--- tests/testremote_storage.py
+++ tests/testremote_storage.py
@@ -10,19 +10,19 @@ class RemoteStorageTestCase(unittest.TestCase):
     VALID_UGC_FILENAME = "steamworkshop/tf2/_thumb.jpg"
     VALID_UGC_URL = "http://cloud-2.steampowered.com/ugc/650994986817657344/D2ADAD7F19BFA9A99BD2B8850CC317DC6BA01BA9/"
 
+    @classmethod
+    def setUpClass(cls):
+        cls._test_file = remote_storage.ugc_file(cls.APPID, cls.VALID_UGCID)
 
     def test_invalid_ugcid(self):
         ugc_file = remote_storage.ugc_file(self.APPID, self.INVALID_UGCID)
         self.assertRaises(remote_storage.FileNotFoundError)
 
     def test_valid_ugcid_filename(self):
-        ugc_file = remote_storage.ugc_file(self.APPID, self.VALID_UGCID)
-        self.assertEqual(ugc_file.filename, self.VALID_UGC_FILENAME)
+        self.assertEqual(self._test_file.filename, self.VALID_UGC_FILENAME)
 
     def test_valid_ugcid_size(self):
-        ugc_file = remote_storage.ugc_file(self.APPID, self.VALID_UGCID)
-        self.assertEqual(ugc_file.size, self.VALID_UGC_SIZE)
+        self.assertEqual(self._test_file.size, self.VALID_UGC_SIZE)
 
     def test_valid_ugcid_url(self):
-        ugc_file = remote_storage.ugc_file(self.APPID, self.VALID_UGCID)
-        self.assertEqual(ugc_file.url, self.VALID_UGC_URL)
+        self.assertEqual(self._test_file.url, self.VALID_UGC_URL)

f51f7400350c3f43563a73835817130ef192f16b
Anthony Garcia <anthony@lagg.me>
2014-09-30 22:19:31 -0700

    Bump version to 4.11, bump copyright year because why not

--- steam/__init__.py
+++ steam/__init__.py
@@ -4,11 +4,11 @@ Copyright (c) 2010-2013, Anthony Garcia <anthony@lagg.me>
 Distributed under the ISC License (see LICENSE)
 """
 
-__version__ = "4.10"
+__version__ = "4.11"
 __author__ = "Anthony Garcia"
 __contact__ = "anthony@lagg.me"
 __license__ = "ISC"
-__copyright__ = "Copyright (c) 2010-2013, " + __author__
+__copyright__ = "Copyright (c) 2010-2014, " + __author__
 
 __all__ = [
     "api", "apps", "items", "loc",

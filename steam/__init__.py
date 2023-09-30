932987b58e66ffd96ebe3ee2f68afeb88ab98009
Anthony Garcia <anthony@lagg.me>
2023-09-30 06:53:17 -0700

    Bump version to 5.0, add build junk to .gitignore

--- steam/__init__.py
+++ steam/__init__.py
@@ -1,14 +1,14 @@
 """
 High level Steam API implementation with low level reusable core
-Copyright (c) 2010-2013, Anthony Garcia <anthony@lagg.me>
+Copyright (c) 2010+, Anthony Garcia <anthony@lagg.me>
 Distributed under the ISC License (see LICENSE)
 """
 
-__version__ = "4.23"
+__version__ = "5.0"
 __author__ = "Anthony Garcia"
 __contact__ = "anthony@lagg.me"
 __license__ = "ISC"
-__copyright__ = "Copyright (c) 2010-2014, " + __author__
+__copyright__ = "Copyright (c) 2010+, " + __author__
 
 __all__ = [
     "api", "apps", "items", "loc",
